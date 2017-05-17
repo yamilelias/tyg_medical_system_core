ad0d0d06c0676c762e5abba03cded58b0bc39fc3
Jonathan Torres Escárcega <A00759281@itesm.mx>
2017-05-17 13:19:09 -0600

    Fixed CSS in Create, Edit, and Delete views of Location Schedule; fixed RedirectToAction of these views as well.

---
+++ system_core_with_authentication/Models/File.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace system_core_with_authentication.Models
+{
+    public class File
+    {
+        public string FileName { get; set; }
+
+    }
+}
